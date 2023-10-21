b935f01b8f9b5da236a619ff5be633f4ba2edcb5
valerii.korniienko <vakor.dev@gmail.com>
2023-10-21 09:22:27 +0300

    +WeaponStrategy +SingleShot

---
+++ Assets/_Project/Scripts/SingleShot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace ShootThemUp._Project.Scripts
+{
+    [CreateAssetMenu(menuName = "ShootThemUp/WeaponStrategy/SingleShot", fileName = "SingleShot", order = 0)]
+    public class SingleShot : WeaponStrategy
+    {
+        public override void Fire(Transform firePoint, LayerMask layerMask)
+        {
+            GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+            projectile.transform.SetParent(firePoint);
+            projectile.layer = layerMask;
+           
+            var projectileComponent = projectile.GetComponent<Projectile>();
+            projectileComponent.SetSpeed(projectileSpeed);
+           
+            Destroy(projectile, projectileLifetime);
+        }
+    }
+}   
---
+++ Assets/_Project/Scripts/WeaponStrategy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace ShootThemUp._Project.Scripts
+{
+    public abstract class WeaponStrategy : ScriptableObject
+    {
+        [SerializeField] private int damage;
+        [SerializeField] private float fireRate;
+        [SerializeField] protected float projectileSpeed;
+        [SerializeField] protected float projectileLifetime;
+        [SerializeField] protected GameObject projectilePrefab;
+        
+        public int Damage => damage;
+        public float FireRate => fireRate;
+
+        public virtual void Initialize()
+        {
+            // no-op
+        }
+
+        public abstract void Fire(Transform firePoint, LayerMask layerMask);
+    }
+}
